0ba0ccce7c3709bdc5459617c508dfc650f56f8b
David Libby <FITSoftware@fluidimaging.com>
2015-02-13 17:40:50 -0500

    Slider with Decade Marks

--- Template/Template/Duity.cpp
+++ Template/Template/Duity.cpp
@@ -1,10 +1,14 @@
 #include "StdAfx.h"
 #include "Duity.h"
+#include <math.h>       /* log */
+
 
 int yLineStart=10;
 int xLineStart=100;
-static int yLineEnd=200;
+static int yLineEnd=400;
 
+static int DASH_STANDARD_WIDTH = 10;
+static int DASH_WIDE_WIDTH = 20;
 
 /**
 
@@ -162,6 +166,9 @@ LRESULT DuityDlg::OnInitDialog(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bH
 	//std::string Start_mL="1.0";
 	//m_EditBoxExample=GetDlgItem(IDC_EDIT_AMOUNT_ML);
 	//m_EditBoxExample.SetWindowTextW(CA2W(Start_mL.c_str()).m_szBuffer);
+	CWindow m_SlideWindowHandle=GetDlgItem(IDC_SLIDER1);
+	RECT Rectangle;
+	m_SlideWindowHandle.GetClientRect(&Rectangle);
 
 	if(m_OnTop)
 	{
@@ -234,11 +241,22 @@ LRESULT DuityDlg::OnPaint(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandle
 	hdc = BeginPaint( &ps); 	
 
 	int LIneLength=yLineEnd-yLineStart;
-	//DrawVerticalLogScale(hdc, yLineStart,yLineEnd,  xLineStart);
+	//DrawVerticalLogScale(hdc, yLineStart,yLineEnd,  400);
 	//DrawVerticalLogScale(hdc, yLineStart+LIneLength+2,yLineEnd+LIneLength*2,  xLineStart);
-	DrawVerticalLogScale(hdc,10,110,   100);
-	DrawVerticalLogScale(hdc, 10,210,  200);
-	DrawVerticalLogScale(hdc, 10,50 , 300);
+	//DrawVerticalLogScale(hdc,10,110,   100);
+	//DrawVerticalLogScale(hdc, 10,210,  200);
+	//DrawVerticalLogScale(hdc, 10,50 , 300);
+
+	//DrawSecadeLogScale(hdc, 20,yLineEnd , 50,1);
+	//DrawSecadeLogScale(hdc, 20,yLineEnd , 100,2);
+	//DrawSecadeLogScale(hdc, 20,yLineEnd , 150,3);
+	//DrawSecadeLogScale(hdc, 20,yLineEnd , 200,4);
+	//DrawSecadeLogScale(hdc, 20,yLineEnd , 250,5);
+	//DrawSecadeLogScale(hdc, 20,yLineEnd , 300,6);
+	//DrawSecadeLogScale(hdc, 20,yLineEnd , 350,7);
+	//DrawSecadeLogScale(hdc, 20,yLineEnd , 400,8);
+	//DrawSecadeLogScale(hdc, 20,yLineEnd , 450,9);
+	//DrawSecadeLogScale(hdc, 20,yLineEnd , 500,10);
 
 	EndPaint( &ps);
 
@@ -246,72 +264,192 @@ LRESULT DuityDlg::OnPaint(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandle
 
 	return 0;
 }
+void DuityDlg::DrawSecadeLogScale(HDC &hdc, int yLineStart,int yLineEnd, int xLineStart,int DecadeCount)
+{
+	LPPOINT lpPoint(0);
+	int LineLength=yLineEnd-yLineStart;
+	int DecadeLength=(int)(LineLength/DecadeCount);
+	int LastLineEnd=yLineStart;
+	for (int ct=1;ct<=DecadeCount;ct++)
+	{
+		DrawVerticalLogScale(hdc, LastLineEnd,LastLineEnd+DecadeLength,xLineStart);
+		
+		MoveToEx (hdc,xLineStart+DASH_WIDE_WIDTH,LastLineEnd,lpPoint);
+		LineTo (hdc,xLineStart,LastLineEnd);
+		
+		RECT DrawArea;
+		DrawArea.top=LastLineEnd-7;
+		DrawArea.bottom=DrawArea.top+15;
+		DrawArea.right=xLineStart+DASH_WIDE_WIDTH-3;
+		DrawArea.left=DrawArea.right+12;
+
+		DrawDecadeText(hdc ,DrawArea,15,"10");
+
+		char str[20];
+		_itoa_s(DecadeCount-ct+1, str, 10);
+		std::string DecadeString= str;
+		RECT LittleDrawArea;
+		LittleDrawArea.bottom=DrawArea.top;
+		LittleDrawArea.left=DrawArea.right+10;
+		LittleDrawArea.top = LittleDrawArea.bottom - 8 ;
+		LittleDrawArea.right=LittleDrawArea.left + 8;
+
+
+
+		DrawDecadeText(hdc ,LittleDrawArea,12,DecadeString);
+
+
+
+		LastLineEnd+=DecadeLength;
+
+	}
+}
+void DuityDlg::DrawDecadeText(HDC &hdc,RECT DrawArea,int size,std::string TextString)
+{
+
+	//DrawArea.top-=19;
+
+	HFONT hFont, hOldFont; 
+
+	// Retrieve a handle to the variable stock font.  
+	hFont = (HFONT)GetStockObject(DEFAULT_GUI_FONT); 
+
+	LOGFONT LargeFont,SmallFont;
+	GetObject ( hFont, sizeof(LOGFONT), &LargeFont );
+	LargeFont.lfHeight = size ;                    // request a 12-pixel-height font
+	_tcsncpy_s(LargeFont.lfFaceName, LF_FACESIZE,  _T("Arial"), 7);
+	SmallFont=LargeFont;
+
+	HFONT hCustomLargeFont = CreateFontIndirect ( &LargeFont );
+	HFONT hCustomSmallFont = CreateFontIndirect ( &SmallFont );
+
+	int gray=110;
+	COLORREF OldCOlor = SetTextColor(hdc, RGB(gray,gray,gray));
+
+	hOldFont = (HFONT)SelectObject(hdc, hCustomLargeFont);
+
+	//Build a 1 pix Gray border by drawing the text sihfted in all 4 directions 
+	int oldCkgroundMode = SetBkMode(hdc, TRANSPARENT);
+	int pixAtound =1;
+	DrawArea.top-=pixAtound;
+	DrawText(hdc,CA2W(TextString.c_str()).m_szBuffer,TextString.size(),&DrawArea,DT_CENTER);
+	DrawArea.top+=pixAtound;
+	DrawArea.left-=pixAtound;
+	DrawText(hdc,CA2W(TextString.c_str()).m_szBuffer,TextString.size(),&DrawArea,DT_CENTER);
+	DrawArea.bottom+=pixAtound;
+	DrawArea.left+=pixAtound;
+	DrawText(hdc,CA2W(TextString.c_str()).m_szBuffer,TextString.size(),&DrawArea,DT_CENTER);
+	DrawArea.bottom-=pixAtound;
+	DrawArea.right+=pixAtound;
+	DrawText(hdc,CA2W(TextString.c_str()).m_szBuffer,TextString.size(),&DrawArea,DT_CENTER);	
+	DrawArea.right-=pixAtound;
+
+	//Now draw the font black Centered.
+	SetTextColor(hdc, RGB(0,0,0));
+	SelectObject(hdc, hCustomSmallFont);
+	DrawText(hdc,CA2W(TextString.c_str()).m_szBuffer,TextString.size(),&DrawArea,DT_CENTER);
+
+	SetBkMode(hdc, oldCkgroundMode  );
+
+	// Restore the original font.        
+	SelectObject(hdc, hOldFont); 
+	// Restore the original Color.        
+	SetTextColor(hdc, OldCOlor);
+
+
+
+}
 
 void DuityDlg::DrawVerticalLogScale(HDC &hdc, int yLineStart,int yLineEnd, int xLineStart)
 {
 	LPPOINT lpPoint(0);
-
-	int divcount=5;
 	
-	int DashWidth=10;
 	int LineLength=yLineEnd-yLineStart;
-	MoveToEx (hdc,xLineStart,10,lpPoint);
+	
+	//DrawCenterLine
+	MoveToEx (hdc,xLineStart,yLineStart,lpPoint);
 	LineTo(hdc,xLineStart,yLineEnd);
-	double y=1.1;
-	//while(y >= yLineStart)
-	int a=0;
-	//double ff=log(y)*(double)LineLength;
-	//double DistMult=pow(double(1.0/5.0),10);
-	double Mult=pow((double)LineLength,1/(double)divcount);
-	int PreviousPix=yLineStart;
 
-	MoveToEx (hdc,xLineStart-DashWidth,yLineStart,lpPoint);
-	LineTo (hdc,xLineStart+DashWidth,yLineStart);
-
-
-	while(a<divcount)
-	//while(y >= yLineStart)
+	double DecadeValue = 10;//Decade Value Must Be 10
+	double CurLineLength=LineLength;
+	int PreviousPix=0;
+	while(DecadeValue >= 1)
 	{
-		
-		a++;		
-		if(yLineEnd==y)
-			DashWidth=20;
-		else
-			DashWidth=10;
-		if(yLineStart+(int)y-PreviousPix>1)
+		double logI=log10((double) DecadeValue );
+		double YPos = LineLength * log10((double) DecadeValue );
+		DecadeValue --;
+		if((yLineEnd-(int)YPos)-PreviousPix>1)
 		{
-			MoveToEx (hdc,xLineStart-DashWidth,yLineStart+(int)y,lpPoint);
-			LineTo (hdc,xLineStart+DashWidth,yLineStart+(int)y);
-			PreviousPix=yLineStart+(int)y;
-		}
-		
-		y=((double)y)*Mult;	
+			MoveToEx (hdc,xLineStart+DASH_STANDARD_WIDTH,yLineEnd-(int)YPos,lpPoint);
+			LineTo (hdc,xLineStart,yLineEnd-(int)YPos);
+			PreviousPix=yLineEnd-(int)YPos;
+			
 
+		}
 	}
-	MoveToEx (hdc,xLineStart-DashWidth*2,yLineEnd,lpPoint);
-	LineTo (hdc,xLineStart+DashWidth*2,yLineEnd);
+
 
 }
+
 
 
 LRESULT DuityDlg::OnBnClickedButton1(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
 {
 	// TODO: Add your control notification handler code here
 
-	while(1)
-	{
-		yLineEnd-=5;
 
-		if(yLineEnd<20)
-			yLineEnd=250;
 
-		//.this->InvalidateRect();
-		::InvalidateRect(this->m_hWnd, 0, true);
-		::UpdateWindow (this->m_hWnd);
-		Sleep(250);
+	yLineEnd-=20;
 
-	}
+	if(yLineEnd<20)
+		yLineEnd=400;
+
+
+	
+	char str[20];
+	_itoa_s(yLineEnd, str, 10);
+	std::string intNumberString= str;
+
+	std::string Display="Height:"+ intNumberString +"\n";
+
+	OutputDebugStringA(Display.c_str());
+	//.this->InvalidateRect();
+	::InvalidateRect(this->m_hWnd, 0, true);
+	::UpdateWindow (this->m_hWnd);
+	Sleep(250);
+
+
+
 
 	return 0;
 }
 
+
+
+LRESULT DuityDlg::OnNMCustomdrawSlider1(int /*idCtrl*/, LPNMHDR pNMHDR, BOOL& /*bHandled*/)
+{
+	LPNMCUSTOMDRAW pNMCD = reinterpret_cast<LPNMCUSTOMDRAW>(pNMHDR);
+	// TODO: Add your control notification handler code here
+
+	// First thing - check the draw stage. If it's the control's prepaint
+	// stage, then tell Windows we want messages for every item.
+
+	pNMCD->hdc;
+	pNMCD->rc.top;
+	CWindow SlideWindowHandle=GetDlgItem(pNMHDR->idFrom);
+	RECT Rectangle;
+	SlideWindowHandle.GetClientRect(&Rectangle);
+
+
+	DrawSecadeLogScale(pNMCD->hdc, Rectangle.top + 14,Rectangle.bottom-15,Rectangle.left+15,4);
+
+	return 0;
+}
+
+
+LRESULT DuityDlg::OnNMReleasedcaptureSlider1(int /*idCtrl*/, LPNMHDR pNMHDR, BOOL& /*bHandled*/)
+{
+	// TODO: Add your control notification handler code here
+
+	return 0;
+}
--- Template/Template/Duity.h
+++ Template/Template/Duity.h
@@ -1,7 +1,6 @@
 #ifndef DUITY_H
 #define DUITY_H
 
-
 #include<TCHAR.H>
 #include <Windows.h>
 #include "resource.h"
@@ -9,6 +8,8 @@
 #include <atlbase.h>
 #include <atlwin.h>
 #include <string>
+#include <vector>
+
 
 class DuityDlg: public CDialogImpl<DuityDlg>
 {
@@ -45,6 +46,8 @@ public:
 		COMMAND_HANDLER(IDOK, BN_CLICKED, OnBnClickedOk)
 		MESSAGE_HANDLER(WM_PAINT, OnPaint)
 		COMMAND_HANDLER(IDC_BUTTON1, BN_CLICKED, OnBnClickedButton1)
+		NOTIFY_HANDLER(IDC_SLIDER1, NM_CUSTOMDRAW, OnNMCustomdrawSlider1)
+		NOTIFY_HANDLER(IDC_SLIDER1, NM_RELEASEDCAPTURE, OnNMReleasedcaptureSlider1)
 	END_MSG_MAP()
 
 
@@ -61,9 +64,14 @@ private:
 public:
 	LRESULT OnPaint(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
 
+	void DrawDecadeText(HDC &hdc,RECT DrawArea,int size,std::string TextString);
 	void DrawVerticalLogScale(HDC &hdc, int yLineStart,int yLineEnd, int xLineStart);
+	void DrawSecadeLogScale(HDC &hdc, int yLineStart,int yLineEnd, int xLineStart,int DecadeNumber);
 
 	LRESULT OnBnClickedButton1(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
+	LRESULT OnNMCustomdrawSlider1(int /*idCtrl*/, LPNMHDR pNMHDR, BOOL& /*bHandled*/);
+	CWindow m_SlideWindowHandle;
+	LRESULT OnNMReleasedcaptureSlider1(int /*idCtrl*/, LPNMHDR pNMHDR, BOOL& /*bHandled*/);
 };
 
 #endif // DUITY_H
