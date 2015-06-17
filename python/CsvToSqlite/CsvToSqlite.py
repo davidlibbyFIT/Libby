2b90b1cb24fbde84b0830e114bcdb1833d2cd29d
David Libby <FITSoftware@fluidimaging.com>
2015-06-17 13:43:14 -0400

    Added in COmmands

--- python/CsvToSqlite/CsvToSqlite.py
+++ python/CsvToSqlite/CsvToSqlite.py
@@ -3,7 +3,15 @@ import fnmatch
 import csv
 import sqlite3
 
-conn = sqlite3.connect('C:\Users\david.libby\Desktop\Python\Harris.db')
+DBName = raw_input("Please enter SqLteDb Name: ")
+print "Your Db = ", DBName
+
+DirToSearch = raw_input("Please enter Directory to search: ")
+print "your Dir", DirToSearch
+
+sys.exit()
+
+conn = sqlite3.connect(DBName)
 
 ##conn.execute('''CREATE TABLE IF NOT EXISTS data (filename text)''')
 conn.execute('''CREATE TABLE IF NOT EXISTS data ("Area (ABD)" REAL, "Area (Filled)" REAL, "Aspect Ratio" REAL, "Average Blue" REAL, "Average Green" REAL, "Average Red" REAL, "BinHigh" INTEGER, "BinLow" INTEGER, "Biovolume (Cylinder)" REAL, "Biovolume (P. Spheroid)" REAL, "Biovolume (Sphere)" REAL, "Calibration Factor" REAL, "Calibration Image" INTEGER, "Camera" INTEGER, "Capture X" INTEGER, "Capture Y" INTEGER, "Ch1 Area" REAL, "Ch1 Peak Area" REAL, "Ch1 Peak" REAL, "Ch1 Width" REAL, "Ch2 Area" REAL, "Ch2 Peak Area" REAL, "Ch2 Peak" REAL, "Ch2 Width" REAL, "Ch2/Ch1 Ratio" REAL, "Circle Fit" REAL, "Circularity (Hu)" REAL, "Circularity" REAL, "Class" TEXT, "Compactness" REAL, "Convex Perimeter" REAL, "Convexity" REAL, "Count" TEXT, "CumulativeFrequency" INTEGER, "CumulativePercentage" REAL, "Date" DATE, "Diameter (ABD)" REAL, "Diameter (ESD)" REAL, "Edge Gradient" REAL, "Elapsed Time" REAL, "Elongation" REAL, "Feret Angle Max" REAL, "Feret Angle Min"  REAL, "Fiber Curl" REAL, "Fiber Straightness" REAL, "Filename" text, "Filter Score" TEXT, "Frequency" INTEGER, "Geodesic Aspect Ratio" REAL, "Geodesic Length" REAL, "Geodesic Thickness" REAL, "Image File" TEXT, "Image Height" INTEGER, "Image Width" INTEGER, "Image X" INTEGER, "Image Y" INTEGER, "Intensity" REAL, "Length" REAL, "List File" TEXT, "List Name" TEXT, "Particle ID" INTEGER, "Particles Per Chain" TEXT, "Perimeter" REAL, "Ratio Blue/Green" REAL, "Ratio Red/Blue" REAL, "Ratio Red/Green" REAL, "Roughness" REAL, "Scatter Area" REAL, "Scatter Peak Area" REAL, "Scatter Peak" REAL, "Scatter Width" REAL, "Sigma Intensity" REAL, "Source Image" INTEGER, "Sphere Complement" REAL, "Sphere Count" REAL, "Sphere Unknown" REAL, "Sphere Volume" REAL, "Sum Intensity" INTEGER, "Symmetry" REAL, "Time" TEXT, "Timestamp" TEXT, "Transparency" REAL, "Volume (ABD)" REAL, "Volume (ESD)" REAL, "Width" REAL );''')
@@ -22,7 +30,7 @@ def find_files(directory, pattern):
 badcount = 0
 Recordcount =0;
 filecount = 0;
-for filename in find_files('C:\Users\david.libby\Desktop\Python', 'data_export.csv'):
+for filename in find_files(DirToSearch, 'data_export.csv'):
     if "_summary.csv" in filename:
         print "Excluding " + filename        
         continue
@@ -73,7 +81,7 @@ for filename in find_files('C:\Users\david.libby\Desktop\Python', 'data_export.c
                 values += ')'
                 startsql += values + ';'
                 Recordcount +=1
-                print "Filecount = " ,filecount,"Rec in file= ",linecount,"Overall Record Count=", Recordcount;
+                print "FN = ",filename," FC = " ,filecount,"Rc = ",linecount,"ORC = ", Recordcount;
                 conn.execute(startsql)
                 #conn.commit()
                 #print startsql
